2dd5e270ff66faf6fb1b2b1089fc5596ca985732
Kyle Kindt <k.kindt8@gmail.com>
2021-03-12 12:18:49 -0500

    commit tree 95df24b2e47ac4984cc5cafcbe8c34a0e2502058

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :words
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |new_words|
+            new_words.split("").sort == @word.split("").sort 
+        end
+    end
+end
